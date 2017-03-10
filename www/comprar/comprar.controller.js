7196d6a971387c54b2f230208931e853c44dc67a
Sebastian Gomez <seagomezar@gmaill.com>
2017-03-10 10:54:00 -0500

    Definiendo los productos, el servicio y añadiendolo al controller

--- www/comprar/comprar.controller.js
+++ www/comprar/comprar.controller.js
@@ -1,4 +1,10 @@
 // comprar.controller.js
-angular.module('starter').controller('ComprarCtrl', function(){
+angular.module('starter').controller('ComprarCtrl', ComprarCtrl);
 
-});
+ComprarCtrl.$inject = ['$scope', 'ProductosService'];
+
+function ComprarCtrl($scope, ProductosService) {
+    ProductosService.lista().then(function (productos) {
+        console.log(productos);
+    });
+}
