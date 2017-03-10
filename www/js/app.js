e95c81fda4c1ddf65faa402545ae9e4c9d6dfc2a
Sebastian Gomez <seagomezar@gmaill.com>
2017-03-09 20:46:08 -0500

    Definiendo el sistema de rutas de nuestra app

--- www/js/app.js
+++ www/js/app.js
@@ -24,62 +24,39 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
-
-  // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
     abstract: true,
-    templateUrl: 'templates/tabs.html'
+    templateUrl: 'tabs/tabs.html'
   })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.comprar', {
+    url: '/comprar',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'comprar': {
+        templateUrl: 'comprar/comprar.html',
+        controller: 'ComprarCtrl as ctrl'
       }
     }
   })
-
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.vender', {
+      url: '/vender',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+        'vender': {
+          templateUrl: 'vender/vender.html',
+          controller: 'VenderCtrl as ctrl'
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
+    .state('tab.cuenta', {
+      url: '/cuenta',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'cuenta': {
+          templateUrl: 'cuenta/cuenta.html',
+          controller: 'CuentaCtrl as ctrl'
         }
       }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+    });
+  $urlRouterProvider.otherwise('/tab/comprar');
 
 });
